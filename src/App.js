ada6958805d8038d8f91b2bfcae9e7e3cbb23de9
Aidan Glickman <aidan@aidanglickman.com>
2022-08-24 00:10:30 -0500

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.scss';
 
 function App() {
@@ -6,9 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 class="tagline">
+        <h1 className="tagline">
           nudge your friends
         </h1>
+        <div className="badges">
+        <a href="https://apps.apple.com/us/">
+          <img className="badge" src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" alt="Download on the App Store"></img>
+        </a>
+        <a href='http://play.google.com/store/'>
+          <img className='badge' alt='Get it on Google Play' src='https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg'></img>
+        </a>  
+        </div>    
       </header>
     </div>
   );
